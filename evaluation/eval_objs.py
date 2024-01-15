c154d911e910adf5a17a0485e92e136b2b60db7c
Kaifeng Zhao <caesarzhao1997@gmail.com>
2024-01-15 11:29:11 +0100

    add eval obj ids

---
+++ evaluation/eval_objs.py
@@ -0,0 +1,15 @@
+selected_objs = {
+    'Armchairs': ['9faefdf6814aaa975510d59f3ab1ed64',
+        'cacb9133bc0ef01f7628281ecb18112',
+        'ea918174759a2079e83221ad0d21775',],
+
+    'L-Sofas': ['5cea034b028af000c2843529921f9ad7',],
+
+    'Sofas': ['1dd6e32097b09cd6da5dde4c9576b854',
+        '71fd7103997614db490ad276cd2af3a4',
+        '277231dcb7261ae4a9fe1734a6086750',],
+
+    'StraightChairs':['2ed17abd0ff67d4f71a782a4379556c7',
+        '68dc37f167347d73ea46bea76c64cc3d',
+        'd93760fda8d73aaece101336817a135f']
+}
